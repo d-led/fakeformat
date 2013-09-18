0211716e1d8949d3bdcac1c38601249a5197c853
d-led <dmlled@yahoo.com>
2013-09-18 18:10:19 +0200

    new beginning

--- fakeformat.hpp
+++ fakeformat.hpp
@@ -6,11 +6,15 @@
 #include <stdlib.h>
 #include <stdexcept>
 
+#include <iostream> //tmp
+
 #ifndef FAKEFORMAT_NO_DEFAULTS
 //configurable
 #include <string>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <utility>
 #endif
 
 namespace ff {
@@ -72,14 +76,16 @@ namespace ff {
 #ifndef FAKEFORMAT_NO_DEFAULTS
 	typedef detail::stream<std::string,std::stringstream> TDefaultStream;
 	typedef std::string TDefaultString;
-	typedef std::string::const_iterator TDefaultPos;
+	typedef size_t TDefaultPos;
 	typedef std::string TDefaultParam;
 	typedef std::vector<TDefaultParam> TDefaultParameters;
 	typedef config<char> TDefaultConfig;
+	typedef std::map< TDefaultPos, std::pair< int,std::map<TDefaultString,TDefaultString> > > TDefaultConfigPara;
 #endif
 
 	template <
 		typename TConfig,
+		typename TConfigPara,
 		typename TStream,
 		typename TString,
 		typename TPos,
@@ -92,126 +98,136 @@ namespace ff {
 		formatter(TString fmt):
 			format_string(fmt)
 		{
+			parse_format_string();
 		}
 
 	public:
 
-		template <typename T>
-		formatter& with(T const& param) {
-			stream.clear();
-			stream.put(param);
-			parameters.push_back(stream.str());
-			return *this;
-		}
+	// 	template <typename T>
+	// 	formatter& with(T const& param) {
+	// 		stream.clear();
+	// 		stream.put(param);
+	// 		parameters.push_back(stream.str());
+	// 		return *this;
+	// 	}
 
-		template <typename T>
-		formatter& also_with(T const& param) {
-			return with(param);
-		}
+	// 	template <typename T>
+	// 	formatter& also_with(T const& param) {
+	// 		return with(param);
+	// 	}
 
-		TString now() const {
-			size_t param_count=parameters.size();
+	// 	TString now() const {
+	// 		size_t param_count=parameters.size();
 
-			if (param_count==0) {
-				return format_string;
-			} else {
-				return parse_and_format();
-			}
-		}
+	// 		if (param_count==0) {
+	// 			return format_string;
+	// 		} else {
+	// 			return parse_and_format();
+	// 		}
+	// 	}
 
-		void clear_parameters() {
-			parameters.clear();
-		}
+	// 	void clear_parameters() {
+	// 		parameters.clear();
+	// 	}
 
-	private:
+	// private:
 
-		TString parse_and_format() const {
-			stream.clear();
+	// 	TString parse_and_format() const {
+	// 		stream.clear();
 
-			TPos last=format_string.begin();
-			for (TPos current=format_string.begin();
-				current!=format_string.end();
-				++current) {
+	// 		TPos last=format_string.begin();
+	// 		for (TPos current=format_string.begin();
+	// 			current!=format_string.end();
+	// 			++current) {
 
-				// predicate can be refactored
-				if (*current==TConfig::scope_begin) {
-					stream.put(last,current);
-					last=current;
-					continue;
-				}
+	// 			// predicate can be refactored
+	// 			if (*current==TConfig::scope_begin) {
+	// 				stream.put(last,current);
+	// 				last=current;
+	// 				continue;
+	// 			}
 
-				// predicate can be refactored
-				if (*current==TConfig::scope_end) {
-					TPos key_beginning=last;
-					++key_beginning;
+	// 			// predicate can be refactored
+	// 			if (*current==TConfig::scope_end) {
+	// 				TPos key_beginning=last;
+	// 				++key_beginning;
 					
-					int key=string_to_key(key_beginning,current);
-					if (valid_range(key)) {
-						stream.put(parameters[key]);
-						//peek
-						++current;
-						last=current;
-						if (current==format_string.end())
-							break;
-						else
-							continue;
-					} else {
-						stream.put(last,current);
-						last=current;
-						continue;
-					}
-				}
+	// 				int key=string_to_key(key_beginning,current);
+	// 				if (valid_range(key)) {
+	// 					stream.put(parameters[key]);
+	// 					//peek
+	// 					++current;
+	// 					last=current;
+	// 					if (current==format_string.end())
+	// 						break;
+	// 					else
+	// 						continue;
+	// 				} else {
+	// 					stream.put(last,current);
+	// 					last=current;
+	// 					continue;
+	// 				}
+	// 			}
+	// 		}
+
+	// 		// finish writing
+	// 		if (last!=format_string.end())
+	// 			stream.put(last,format_string.end());
+
+	// 		return stream.str();
+	// 	}
+
+	// 	bool valid_range(int key) const {
+	// 		return key>=0 && (size_t)key<parameters.size();
+	// 	}
+
+	// 	static int string_to_key(TPos beg,TPos end) {
+	// 		int res=-1;
+	// 		try {
+	// 			//configure?
+	// 			res=std::stoi(TString(beg,end))
+	// 				-TConfig::index_begin;
+	// 		} catch (std::exception&) {}
+	// 		return res;
+	// 	}
+	private:
+		void parse_format_string() {
+			TPos length=format_string.length();
+			for (TPos pos = 0; pos < length; ++pos) {
+				std::cout<<format_string[pos];
 			}
-
-			// finish writing
-			if (last!=format_string.end())
-				stream.put(last,format_string.end());
-
-			return stream.str();
-		}
-
-		bool valid_range(int key) const {
-			return key>=0 && (size_t)key<parameters.size();
-		}
-
-		static int string_to_key(TPos beg,TPos end) {
-			int res=-1;
-			try {
-				//configure?
-				res=std::stoi(TString(beg,end))
-					-TConfig::index_begin;
-			} catch (std::exception&) {}
-			return res;
+			std::cout<<std::endl;
 		}
 	
-	private:
+	// private:
 		TString format_string;
-		TParameters parameters;
+	// 	TParameters parameters;
 
-		TStream stream; // Caution: the stream is shared within the implementation!
+	// 	TStream stream; // Caution: the stream is shared within the implementation!
 	};
 
 #ifndef FAKEFORMAT_NO_DEFAULTS
-	formatter<TDefaultConfig,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters> format(TDefaultString fmt) {
-		return formatter<TDefaultConfig,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters>(fmt);
+	formatter<TDefaultConfig,TDefaultConfigPara,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters> format(TDefaultString fmt) {
+		return formatter<TDefaultConfig,TDefaultConfigPara,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters>(fmt);
 	}
 
 	template <typename TConfig>
-	formatter<TConfig,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters> format(TDefaultString fmt) {
-		return formatter<TConfig,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters>(fmt);
+	formatter<TConfig,TDefaultConfigPara,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters> format(TDefaultString fmt) {
+		return formatter<TConfig,TDefaultConfigPara,TDefaultStream,TDefaultString,TDefaultPos,TDefaultParam,TDefaultParameters>(fmt);
 	}
 #endif
 
 	template <
 		typename TConfig,
+		typename TConfigPara,
 		typename TStream,
 		typename TString,
 		typename TPos,
 		typename TParam,
 		typename TParameters
 	>
-	formatter<TConfig,TStream,TString,TPos,TParam,TParameters> format(TString fmt) {
-		return formatter<TConfig,TStream,TString,TPos,TParam,TParameters>(fmt);
+	formatter<TConfig,TConfigPara,TStream,TString,TPos,TParam,TParameters> format(TString fmt) {
+		return formatter<TConfig,TConfigPara,TStream,TString,TPos,TParam,TParameters>(fmt);
 	}
 
 }
